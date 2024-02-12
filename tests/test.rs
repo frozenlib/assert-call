7687ca8dd74a74102030fae15102d5ee56a513b2
frozenlib <frozenlib@users.noreply.github.com>
2024-02-12 11:00:59 +0900

    Simplify.

--- tests/test.rs
+++ tests/test.rs
@@ -10,18 +10,18 @@ use assert_call::{call, Call, CallRecorder};
 
 #[test]
 fn new() {
-    let mut cr = CallRecorder::new();
+    let mut c = CallRecorder::new();
     call!("1");
 
-    cr.verify("1");
+    c.verify("1");
 }
 
 #[test]
 fn new_thread() {
-    let mut cr = CallRecorder::new();
+    let mut c = CallRecorder::new();
     spawn(|| call!("1")).join().unwrap();
 
-    cr.verify("1");
+    c.verify("1");
 }
 
 #[test]
@@ -44,10 +44,10 @@ fn new_parallel() {
 
 #[test]
 fn new_local() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
 
-    cr.verify("1");
+    c.verify("1");
 }
 
 #[should_panic]
@@ -60,169 +60,169 @@ fn new_local_nested() {
 #[should_panic]
 #[test]
 fn extra_call() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
-    cr.verify(());
+    c.verify(());
 }
 
 #[should_panic]
 #[test]
 fn not_call() {
-    let mut cr = CallRecorder::new_local();
-    cr.verify("1");
+    let mut c = CallRecorder::new_local();
+    c.verify("1");
 }
 
 #[should_panic]
 #[test]
 fn not_expect() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
-    cr.verify(());
+    c.verify(());
 }
 
 #[test]
 fn verify_2() {
-    let mut cr = CallRecorder::new();
+    let mut c = CallRecorder::new();
     call!("1");
     call!("2");
-    cr.verify(["1", "2"]);
+    c.verify(["1", "2"]);
 
     call!("3");
     call!("4");
-    cr.verify(["3", "4"]);
+    c.verify(["3", "4"]);
 }
 
 #[test]
 fn verify_2_local() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
     call!("2");
-    cr.verify(["1", "2"]);
+    c.verify(["1", "2"]);
 
     call!("3");
     call!("4");
-    cr.verify(["3", "4"]);
+    c.verify(["3", "4"]);
 }
 
 #[test]
 fn seq_array() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
     call!("2");
     call!("3");
 
-    cr.verify(["1", "2", "3"]);
+    c.verify(["1", "2", "3"]);
 }
 
 #[test]
 fn seq_vec() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
     call!("2");
     call!("3");
 
-    cr.verify(vec!["1", "2", "3"]);
+    c.verify(vec!["1", "2", "3"]);
 }
 
 #[should_panic]
 #[test]
 fn seq_fail_not_call() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("1");
-    cr.verify(["1", "2"]);
+    c.verify(["1", "2"]);
 }
 
 #[should_panic]
 #[test]
 fn seq_fail_wrong_order() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     call!("2");
     call!("1");
-    cr.verify(["1", "2"]);
+    c.verify(["1", "2"]);
 }
 
 #[test]
 fn any() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::any(["1", "2", "3"]);
     call!("1");
-    cr.verify(&expect);
+    c.verify(&expect);
     call!("3");
-    cr.verify(&expect);
+    c.verify(&expect);
     call!("2");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[should_panic]
 #[test]
 fn any_fail_not_match() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::any(["1", "2", "3"]);
     call!("4");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[should_panic]
 #[test]
 fn any_fail_not_call() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::any(["1", "2", "3"]);
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[should_panic]
 #[test]
 fn any_fail_extra_call() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::any(["1", "2", "3"]);
     call!("1");
     call!("2");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[test]
 fn any_seq() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::any([["1", "2"], ["a", "b"]]);
     call!("a");
     call!("b");
-    cr.verify(&expect);
+    c.verify(&expect);
     call!("1");
     call!("2");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[test]
 fn par() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::par(["1", "2", "3"]);
     call!("1");
     call!("2");
     call!("3");
-    cr.verify(&expect);
+    c.verify(&expect);
 
     call!("3");
     call!("1");
     call!("2");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[test]
 fn par_seq() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let expect = Call::par([["1", "2"], ["a", "b"]]);
     call!("1");
     call!("a");
     call!("b");
     call!("2");
-    cr.verify(&expect);
+    c.verify(&expect);
 }
 
 #[test]
 fn call_format() {
-    let mut cr = CallRecorder::new_local();
+    let mut c = CallRecorder::new_local();
     let x = 10;
     call!("a_{x}");
     call!("b_{}", x);
-    cr.verify(["a_10", "b_10"]);
+    c.verify(["a_10", "b_10"]);
 }
